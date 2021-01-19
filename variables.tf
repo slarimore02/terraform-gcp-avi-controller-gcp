152766bee2846fe9917c3de84672e767d6847a67
Sean Larimore <slarimore02@gmail.com>
2021-01-19 07:41:26 -0700

    Add stronger password validation

--- variables.tf
+++ variables.tf
@@ -64,7 +64,7 @@ variable "controller_password" {
   type        = string
   sensitive   = true
   validation {
-    condition     = length(var.controller_password) > 7
+    condition     = length(var.controller_password) > 7 && can(regex("^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])$", var.controller_password))
     error_message = "The controler_password value must be more than 8 characters and contain at least one each of uppercase, lowercase, numbers, and special characters."
   }
 }
